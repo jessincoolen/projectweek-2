dd69c8b5e37323bcbe6b78b42e71ddc6a0c033d3
Ole2710 <56429293+Ole2710@users.noreply.github.com>
2020-01-27 21:29:28 +0100

    Update Clickable_Order.js

--- 1/Puzzel 1 Javascript/Clickable_Order.js
+++ 1/Puzzel 1 Javascript/Clickable_Order.js
@@ -1,44 +1,67 @@
 // Variables
 
 let icon1 = false;
-const order1 = 1;
 
 let icon2 = false;
-const order2 = 2;
 
 let icon3 = false;
-const order3 = 3;
 
 let icon4 = false;
-const order4 = 4;
 
 let clicked = 0;
 
 // On Click Functions
 
 function Icon1(){
-    if(clicked = 0){
+    if(clicked == 0){
         icon1 = true;
         clicked++;
+    }
+    else{
+        clicked = 0;
+        icon1 = false;
+        icon2 = false;
+        icon3 = false;
+        icon4 = false;
+        console.log("Level Failed")
+        console.log(clicked + icon1 + icon2 + icon3 + icon4)
     }
 }
 
 function Icon2(){
-    if(clicked = 1){
+    if(clicked == 1){
         icon2 = true;
         clicked++;
+    }
+    else{
+        clicked = 0;
+        icon1 = false;
+        icon2 = false;
+        icon3 = false;
+        icon4 = false;
+        console.log("Level Failed")
+        console.log(clicked + icon1 + icon2 + icon3 + icon4)
     }
 }
 
 function Icon3(){
-    if(clicked = 2){
+    if(clicked == 2){
         icon3 = true;
         clicked++;
+    }
+    else{
+        clicked = 0;
+        icon1 = false;
+        icon2 = false;
+        icon3 = false;
+        icon4 = false;
+        console.log("Level Failed")
+        console.log(clicked + icon1 + icon2 + icon3 + icon4)
     }
 }
 
 function Icon4(){
-    if(clicked = 3){
+    if(clicked == 3){
         icon4 = true;
         clicked = 0;
         icon1 = false;
@@ -46,5 +69,15 @@ function Icon4(){
         icon3 = false;
         icon4 = false;
         console.log("Level Completed")
+        console.log(clicked + icon1 + icon2 + icon3 + icon4)
+    }
+    else{
+        clicked = 0;
+        icon1 = false;
+        icon2 = false;
+        icon3 = false;
+        icon4 = false;
+        console.log("Level Failed")
+        console.log(clicked + icon1 + icon2 + icon3 + icon4)
     }
 }
