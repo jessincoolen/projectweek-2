3850d63a9ed3b20d81d72ba8dd331d17e0449beb
Luc34521 <53125507+Luc34521@users.noreply.github.com>
2020-01-29 16:36:08 +0100

    Aangepaste index en succes pagina

--- index.php
+++ index.php
@@ -14,13 +14,14 @@ and open the template in the editor.
             this.top.location !== this.location && (this.top.location = this.location);
         </script>
         <style>
-            a   {color: black;}
+            a   {color: white;}
+            h1  {color: white;}
         </style>
         <!-- UNTIL HERE -->
     </head>
-    <body background='./images/metal.jpg'>
+    <body background='./images/bombdefusingbkg.jpg'>
     <center>
-        <table width='40%' bgcolor='whitesmoke'>
+        <table width='40%' bgcolor='black'>
             <tr><td align='center'><h1><b>BOMB DEFUSAL SQUAD</B><h1></td></tr>
             <tr><td align='center'><a href='frameslevel1.php'><h2>Level 1</h2></a></td></tr>
             <tr><td align='center'><a href='frameslevel2.php'><h2>Level 2</h2></a></td></tr>
