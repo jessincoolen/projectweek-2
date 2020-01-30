537e4bb1a8b9faf5fcf75291308321066ae3ace6
Jaymian-Lee <404767@arcuscollege.nl>
2020-01-30 15:30:35 +0100

    Added all the final files
    Alles zit e rnu in inclusief de handleiding om de puzzels te voltooien

--- 2/frameslevel3.php
+++ 2/frameslevel3.php
@@ -33,11 +33,11 @@ The basic frameset filled with the level 1 components
                 </frameset>
                 <frameset rows='50%,50%' border='10'>
                     <frame name='center-middle-top' src='./nopuzzleframe.html' noresize scrolling='no'>					<!-- Casing: center top / puzzle game 2 -->
-                    <frame name='center-middle-bottom' src='puzzel_4/puzzel_2_a4_completed.html' noresize scrolling='no'>				<!-- Casing: center bottom / puzzle frame 3 -->
+                    <frame name='center-middle-bottom' src='puzzel_2/puzzel_2_a2_completed.html' noresize scrolling='no'>				<!-- Casing: center bottom / puzzle frame 3 -->
                 </frameset>
                 <frameset rows='50%,50%' border='10'>
                     <frame name='center-right-top' src='./nopuzzleframe.html' noresize scrolling='no'>					<!-- Casing: upper right corner / puzzle frame 4 -->
-                    <frame name='center-right-bottom' src='puzzel_3/puzzel_2_a3.html' noresize scrolling='no'>				<!-- Casing: lower right corner / puzzle frame 5 -->
+                    <frame name='center-right-bottom' src='puzzel_4/puzzel_2_a4.html' noresize scrolling='no'>				<!-- Casing: lower right corner / puzzle frame 5 -->
                 </frameset>
                 <frame name='casing-rigth' src='./casingborder.html' noresize scrolling='no'>							<!-- Casing: border right -->
             </frameset>
